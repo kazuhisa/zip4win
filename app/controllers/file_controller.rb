77c70e5ecfd15e441d302c794bddfed62401f837
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2016-10-02 17:34:42 +0900

    ひとまずできた

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -2,6 +2,7 @@ require 'zip'
 
 class FileController < ApplicationController
   def index
+    @password = ENV['PASSWORD'].present? ? Date.today.strftime(ENV['PASSWORD']) : ""
     @zip_id = SecureRandom.hex(16)
     gon.zip_id = @zip_id
   end
@@ -17,8 +18,12 @@ class FileController < ApplicationController
   end
 
   def download
-    encrypter = Zip::TraditionalEncrypter.new("password")
     zip_id = params[:id]
+    unless File.exist?(File.join("/tmp/", zip_id))
+      redirect_to :file_index
+      return
+    end
+    encrypter = params[:password].present? ? Zip::TraditionalEncrypter.new(params[:password]) : nil
     io = StringIO.new('')
     io.set_encoding(Encoding::CP932)
     buffer = Zip::OutputStream.write_buffer(io, encrypter) do |zos|
